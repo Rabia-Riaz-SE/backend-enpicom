51766cde743844e67a528a62a1bcb9e12fae4b36
Rabia-Riaz-SE <Rabiariaz44@gmail.com>
2024-09-10 03:02:31 +0200

    Refactor controller code

--- src/dna/dna.controller.ts
+++ src/dna/dna.controller.ts
@@ -1,31 +1,31 @@
 import { Controller, Get, Param, Body, Post, Query } from '@nestjs/common';
-import {DNAService} from './dna.service';
-import {DNADto, CreateDNADto} from './dto'
+import { DNAService } from './dna.service';
+import { CreateDNADto } from './dto'
 
 @Controller('dna')
 export class DNAController {
-    constructor(private readonly DNAService: DNAService) {}
+  constructor(private readonly DNAService: DNAService) { }
 
-      /**
-   * we have used get decorator with search query to get matched DNAs from request
-   * so the API URL will be
-   * Get http://localhost:3000/dna/dna=searchKey&levenshtein=1
-   */
-    @Get()
-    find(@Query("search") search:string , @Query("levenshtein") levenshtein?: string){
-      const result =   this.DNAService.find({DNA:search || "", levenshtein: levenshtein});
-      return result
-    }
+  /**
+* we have used get decorator with search query to get matched DNAs from request
+* so the API URL will be
+* Get http://localhost:3000/dna/dna=searchKey&levenshtein=1
+*/
+  @Get()
+  find(@Query("search") search: string, @Query("levenshtein") levenshtein?: string) {
+    const result = this.DNAService.find({ DNA: search, levenshtein: levenshtein });
+    return result
+  }
 
-       /**
-   * we have used Post decorator with Dna as string in an object to insert new DNA to system
-   * so the API URL will be
-   * Post http://localhost:3000/dna
-   */
-    @Post()
-    create(@Body() createDNA: CreateDNADto ) {
-        const result  = this.DNAService.create(createDNA);
-        return result;
-    }
+  /**
+* we have used Post decorator with Dna as string in an object to insert new DNA to system
+* so the API URL will be
+* Post http://localhost:3000/dna
+*/
+  @Post()
+  create(@Body() createDNA: CreateDNADto) {
+    const result = this.DNAService.create(createDNA);
+    return result;
+  }
 }
 
