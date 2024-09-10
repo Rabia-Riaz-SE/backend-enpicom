62dbfe80e70ba6ee848a67f114cc44cb1f8f49c0
Rabia-Riaz-SE <Rabiariaz44@gmail.com>
2024-09-10 03:04:12 +0200

    Refactor service  code

--- src/dna/dna.service.ts
+++ src/dna/dna.service.ts
@@ -7,10 +7,12 @@ import Utils from '../utils'
 
 @Injectable()
 export class DNAService {
-    constructor( @InjectRepository(DNA) private readonly dnaRepository: Repository<DNA>) { }
+    constructor(@InjectRepository(DNA) private readonly dnaRepository: Repository<DNA>) { }
 
     async find(searchDNA: SearchDNADto): Promise<DNADto[]> {
         try {
+            searchDNA.DNA = searchDNA?.DNA.toUpperCase() ?? '';
+
             // Validate levenshtein distance is a number 
             if (searchDNA?.levenshtein && isNaN(+(searchDNA?.levenshtein)))
                 throw new HttpException('levenshtein must be a number', HttpStatus.BAD_REQUEST);
@@ -18,12 +20,12 @@ export class DNAService {
             // Find and calculate levenshtein distance between search parameter and DB existing DNAs 
             if (searchDNA.levenshtein) {
                 const allDNAs = await this.dnaRepository.find();
-                return allDNAs.filter(val => +searchDNA.levenshtein === Utils.LevenshteinDistance(val.DNA, searchDNA.DNA.toUpperCase()));
+                return allDNAs.filter(val => +searchDNA.levenshtein === Utils.LevenshteinDistance(val.DNA, searchDNA.DNA));
             }
 
             // if levenshtein distance not provided return only searched DNAs
             return await this.dnaRepository.find({
-                where: { DNA: Like(`${searchDNA.DNA.toUpperCase()}%`) },
+                where: { DNA: Like(`${searchDNA.DNA}%`) },
             });
         }
         catch (err) {
@@ -37,10 +39,10 @@ export class DNAService {
             const DNAValue = createDNA.DNA.toUpperCase();
             const entity = await this.dnaRepository.findOne({
                 where: { DNA: DNAValue }
-              });
+            });
             if (entity)
                 throw new HttpException(`${DNAValue} already exists `, HttpStatus.CONFLICT);
-            return await this.dnaRepository.save({...createDNA, DNA: DNAValue});
+            return await this.dnaRepository.save({ ...createDNA, DNA: DNAValue });
         }
         catch (err) {
             throw new HttpException(err.message || 'Internal Server Error', err.status || HttpStatus.INTERNAL_SERVER_ERROR);
